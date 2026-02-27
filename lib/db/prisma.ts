4ad4b2a47eac7fc7eb11beca2ac7c8c244b5a9dc
BenCoTHPS <benco.webdev@gmail.com>
2026-02-27 21:47:09 +0000

    Cleaned up db stuff, updated readme

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@/generated/prisma/client/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 // 2. We pass the raw string directly. In production, your Electron main.js will overwrite this dynamically.
-const connectionString = process.env.DATABASE_URL || 'file:./prisma/dev.db';
+const connectionString = process.env.DATABASE_URL || 'file:./prisma/development.db';
 
 // 3. The adapter now takes a simple configuration object with the url
 const adapter = new PrismaBetterSqlite3({
